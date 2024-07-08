62c1f9c95dd7743b13099954ed2c819d333b1b32
harish-hearty <harish0234k@gmail.com>
2024-07-08 09:31:52 +0530

    deployment test

--- src/test/java/com/GuideIn/GuideInApplicationTests.java
+++ src/test/java/com/GuideIn/GuideInApplicationTests.java
@@ -4,11 +4,19 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.TestPropertySource;
 
 import jakarta.activation.DataSource;
 
 @SpringBootTest
 @ActiveProfiles("test")
+@TestPropertySource(properties = {
+        "spring.datasource.url=jdbc:mysql://localhost:3306/testdb",
+        "spring.datasource.username=testuser",
+        "spring.datasource.password=testpassword",
+        "spring.datasource.driver-class-name=com.mysql.cj.jdbc.Driver",
+        "spring.jpa.hibernate.ddl-auto=create-drop"
+})
 class GuideInApplicationTests {
 	
     @MockBean
