cb50f5d4dbf4e21917fd56d44c3a9d8f77cf6053
harish-hearty <harish0234k@gmail.com>
2024-07-08 14:30:44 +0530

    deployment test

--- src/test/java/com/GuideIn/GuideInApplicationTests.java
+++ src/test/java/com/GuideIn/GuideInApplicationTests.java
@@ -2,11 +2,6 @@ package com.GuideIn;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.TestPropertySource;
-
-import jakarta.activation.DataSource;
 
 @SpringBootTest
 class GuideInApplicationTests {
