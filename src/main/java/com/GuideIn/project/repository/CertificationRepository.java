449b6bfe22e0a6e54640f465bf61e25a05668ee4
Guidein7 <atem.learning@gmail.com>
2025-07-06 15:38:43 +0530

    Update CertificationRepository.java  certification repository changes

--- src/main/java/com/GuideIn/project/repository/CertificationRepository.java
+++ src/main/java/com/GuideIn/project/repository/CertificationRepository.java
@@ -13,8 +13,8 @@ import com.GuideIn.project.model.Certification;
 @Repository
 public interface CertificationRepository extends JpaRepository<Certification,Long>{
 	
-	@Query(value="select * from certifications where ?1 is null or course_category=?1",nativeQuery=true)
-	Page<Certification> getdataByFilter(String courseCategory, Pageable pageable);
+	@Query(value="select * from certifications where (?1 is null or course_category=?1) and (?2 is null or course_duration=?2)",nativeQuery=true)
+	Page<Certification> getdataByFilter(String courseCategory, String duration, Pageable pageable);
 	
 	@Query(value="select distinct course_category from certifications",nativeQuery=true)
 	List<String> getdistinctCourseCatogory();
