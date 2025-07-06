f7c86a2e12dfba30e079f69bd39123564a1fbf84
Guidein7 <atem.learning@gmail.com>
2025-07-06 15:39:29 +0530

    Update CourseInfoRepository.java course repository changes

--- src/main/java/com/GuideIn/project/repository/CourseInfoRepository.java
+++ src/main/java/com/GuideIn/project/repository/CourseInfoRepository.java
@@ -13,8 +13,8 @@ import com.GuideIn.project.model.CourseInfo;
 @Repository
 public interface CourseInfoRepository extends JpaRepository<CourseInfo,Long>{
 	
-	@Query(value="select * from course_info where ?1 is null or location =?1 and ?2 is null or mode_of_class=?2",nativeQuery=true)
-	Page<CourseInfo> findByFilters(String location, String modeOfClass, Pageable pageable);
+	@Query(value="select * from course_info where (?1 is null or location =?1) and (?2 is null or mode_of_class=?2) and (?3 is null or course_duration=?3) and (?4 IS NULL OR course_name LIKE CONCAT('%', ?4, '%'))  or (?5 IS NULL OR institute_name LIKE CONCAT('%', ?5, '%'))",nativeQuery=true)
+	Page<CourseInfo> findByFilters(String location, String modeOfClass, String duration, String courseName, String instituteName, Pageable pageable);
 	
 	@Query(value="select location,estimated_course_price,course_duration,mode_of_class from course_info",nativeQuery=true)
 	List<String> getDistinctdropDowns();
