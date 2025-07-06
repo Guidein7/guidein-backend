2bb93eddbbe318b576391473ea2cb5665c162772
Guidein7 <atem.learning@gmail.com>
2025-07-06 15:40:06 +0530

    Update YouTubeVideoRepository.java repository changes

--- src/main/java/com/GuideIn/project/repository/YouTubeVideoRepository.java
+++ src/main/java/com/GuideIn/project/repository/YouTubeVideoRepository.java
@@ -13,8 +13,8 @@ import com.GuideIn.project.model.YouTubeVideo;
 @Repository
 public interface YouTubeVideoRepository extends JpaRepository<YouTubeVideo,Long>{
 	
-	@Query(value="select * from youtube_vedio where ?1 is null or topic=?1",nativeQuery=true)
-	Page<YouTubeVideo> findByTopic(String topic, Pageable pageable);
+	@Query(value="select * from youtube_vedio where (?1 is null or topic=?1) and (?2 is null or duration=?2) and (?3 IS NULL OR video_title LIKE CONCAT('%', ?3, '%'))",nativeQuery=true)
+	Page<YouTubeVideo> findByTopic(String topic, String duration, String vedioTitle, Pageable pageable);
 	
 	
 	
@@ -28,5 +28,11 @@ public interface YouTubeVideoRepository extends JpaRepository<YouTubeVideo,Long>
 
 	@Query(value="select distinct duration  from youtube_vedio",nativeQuery=true)
 	List<String> getdistictduration();
+
+
+
+
+	@Query(value="select distinct tags from youtube_vedio",nativeQuery=true)
+	List<String> getdistinctTags();
 
 }
